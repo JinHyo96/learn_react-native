d42d9c5ccfe9f81bc8809bbd2bd36bd3ba590a80
Jinyo <ars723@naver.com>
2020-10-18 15:24:35 +0900

    기초적인 내용 완료?

--- Weather.js
+++ Weather.js
@@ -7,43 +7,63 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 const weatherOptions = {
     Thunderstorm: {
         iconName: "weather-lightning",
-        gradient: ["#373B44", "#4286f4"]
+        gradient: ["#373B44", "#4286f4"],
+        title: "Thunderstorm",
+        subtitle: "Just dont go outside"
       },
       Drizzle: {
         iconName: "weather-hail",
-        gradient: ["#89F7FE", "#66A6FF"]
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "Just dont go outside"
       },
       Rain: {
         iconName: "weather-rainy",
-        gradient: ["#00C6FB", "#005BEA"]
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "Rain",
+        subtitle: "지금 밖에 비오고 있는데요?"
       },
       Snow: {
         iconName: "weather-snowy",
-        gradient: ["#7DE2FC", "#B9B6E5"]
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "Snow",
+        subtitle: "눈 오고 있음.. 부산이면 위험해"
       },
       Atmosphere: {
         iconName: "weather-hail",
-        gradient: ["#89F7FE", "#66A6FF"]
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Atmosphere",
+        subtitle: "아무말이나 적어보려고 그냥 ㅋㅋ"
       },
       Clear: {
         iconName: "weather-sunny",
-        gradient: ["#FF7300", "#FEF253"]
+        gradient: ["#FF7300", "#FEF253"],
+        title: "Clear",
+        subtitle: "weather-sunny weather-sunny weather-sunny weather-sunny"
       },
       Clouds: {
         iconName: "weather-cloudy",
-        gradient: ["#D7D2CC", "#304352"]
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "weather-cloudy weather-cloudy weather-cloudy weather-cloudy"
       },
       Mist: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Mist",
+        subtitle: "weather-hail weather-hail"
       },
       Dust: {
         iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Dust",
+        subtitle: "weather-hail weather-hail weather-hail"
       },
       Haze: {
         iconName: "weather-hail",
         gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "Just dont go outside"
       }
   
 };
@@ -51,22 +71,24 @@ const weatherOptions = {
 export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
-      colors={weatherOptions[condition].gradient}
-      style={styles.container}
-    >
+      colors={weatherOptions["Snow"].gradient}
+      style={styles.container}>
       <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
         <MaterialCommunityIcons
           size={96}
-          name={weatherOptions[condition].iconName}
-          color="white"
-        />
-        <Text style={styles.temp}>{temp}°</Text>
+          name={weatherOptions["Snow"].iconName}
+          color="white"/>
+        <Text style={styles.temp}>{temp}°c</Text>
       </View>
-      <View style={styles.halfContainer} >
+      
+      <View style={{...styles.halfContainer, ...styles.textContainer}} >
+        <Text style={styles.title}>{weatherOptions["Snow"].title}</Text>
+        <Text style={styles.subtitle}>{weatherOptions["Snow"].subtitle}</Text>
       </View>
     </LinearGradient>
   );
+  //... 두개의 오브젝트를 함께 쓰는 es6 방식
 }
 
 Weather.propTypes = {
@@ -89,5 +111,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  title:{
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10
+  },
+  subtitle:{
+    color: "white",
+    fontWeight: "900",
+    fontSize: 24
+  },
+  textContainer:{
+    paddingHorizontal:20,
+    alignItems: "flex-start"
   }
 });
